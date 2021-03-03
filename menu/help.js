a9f8b87600dcff526df1d3d0f477d0e30ad7c391
PUDIDI <74692006+X-PrCx12@users.noreply.github.com>
2021-03-03 17:43:45 +0700

    Update help.js

--- menu/help.js
+++ menu/help.js
@@ -111,7 +111,7 @@ ${a}UNTUK FITUR TERUPDATE SILAHKAN KETIK${a} *${prefix}infobot*
 │➲ ${a}PENYEDIA REST API${a}
 │➲ ${a}CONTENT CREATOR BOT WHATSAPP${a}
 ┬
-╰──────≽ *Created © FXC7*`
+╰──────≽ *EPEP*`
 }
 
 exports.help = help
@@ -206,13 +206,13 @@ const donasi = (name) => {
 	return `       
 ╭─≽「 *DONASI AGAR BOT TETAP ONLINE* 」
 ┴
-│✓ *Trakteer: https://trakteer.id/FarhanXCo*
-│✓ *Saweria: https://saweria.co/FarhanXCo*
+│✓ *Trakteer: kentod*
+│✓ *Saweria: saweria kentod*
 │
-│✓ ${a}Pulsa: 08311800241${a}
+│✓ ${a}Pulsa: 081260899819${a}
 │ *15k Invite Bot Ke Grup selama 1 bulan*
 │
-│✓ ${a}OVO: 08311800241${a}
+│✓ ${a}OVO: kentod${a}
 │ *10k Invite Bot Ke Group Selama 1 Bulan*
 ┬
 ╰─────≽「 *BY ${name}* 」
